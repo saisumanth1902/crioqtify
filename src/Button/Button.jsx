4b0ebeccca0e5a6ff574267f8c35cd421837b5af
Sai Sumanth <saisumanth@Sais-MacBook-Pro.local>
2024-11-01 14:07:33 +0530

    Added Required CSS for Feedback Button

--- src/Button/Button.jsx
+++ src/Button/Button.jsx
@@ -3,8 +3,11 @@ import styles from "./Button.module.css";
 
 export default function Button({ children, onClick }) {
   return (
-    <button className={styles.button} onClick={onClick}>
+    <button className={styles.feedbackButton} onClick={onClick}>
       {children}
     </button>
   );
 }
+
+
+

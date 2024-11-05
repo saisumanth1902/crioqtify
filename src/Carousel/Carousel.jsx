64fab79ebe17a81f2beed49b10e3d223f9fc880d
Sai Sumanth <saisumanth@Sais-MacBook-Pro.local>
2024-11-05 22:42:48 +0530

    Rectified Error

--- src/Carousel/Carousel.jsx
+++ src/Carousel/Carousel.jsx
@@ -1,21 +1,21 @@
-// src/Carousel/Carousel.jsx
-import React from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/swiper-bundle.css';
+// // src/Carousel/Carousel.jsx
+// import React from 'react';
+// import { Swiper, SwiperSlide } from 'swiper';
+// import 'swiper/swiper-bundle.css';
 
-const Carousel = ({ items }) => {
-  return (
-    <Swiper
-      spaceBetween={20}
-      slidesPerView={3} // Adjust based on screen size
-    >
-      {items.map((item, index) => (
-        <SwiperSlide key={index}>
-          {item} {/* Each item should be a valid React component */}
-        </SwiperSlide>
-      ))}
-    </Swiper>
-  );
-};
+// const Carousel = ({ items }) => {
+//   return (
+//     <Swiper
+//       spaceBetween={20}
+//       slidesPerView={3} // Adjust based on screen size
+//     >
+//       {items.map((item, index) => (
+//         <SwiperSlide key={index}>
+//           {item} {/* Each item should be a valid React component */}
+//         </SwiperSlide>
+//       ))}
+//     </Swiper>
+//   );
+// };
 
-export default Carousel;
+// export default Carousel;
